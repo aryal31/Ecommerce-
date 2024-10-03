a8b260c6402efc66e14c95e58339e3ddb3213089
aryal31 <bipinaryal11@gmail.com>
2024-10-03 16:33:22 +0545

    My account page created

--- src/Pages/Myaccount.jsx
+++ src/Pages/Myaccount.jsx
@@ -4,13 +4,52 @@ const Myaccount = () => {
   return (
     <div>
       <div className='text-[20px] text-[#DB4444] font-semibold'>Edit your profile</div>
-      <div className='flex'>
-        <div>
+      <div className='flex gap-[40px]'>
+        <div className='flex flex-col'>
           <label htmlFor="fname">First Name</label>
-          <input type="text" id='fname' />
+          <input
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            type="text"
+            id="fname"
+            required
+          />
         </div>
-        <div></div>
-      </div>
+        <div>
+        <div className='flex flex-col'>
+          <label htmlFor="fname">First Name</label>
+          <input
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            type="text"
+            id="fname"
+            required
+          />
+        </div>
+        </div>
+        </div>
+        <div className='flex gap-[40px]'>
+        <div className='flex flex-col'>
+          <label htmlFor="fname">Email</label>
+          <input
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            type="text"
+            id="fname"
+            required
+          />
+        </div>
+        <div>
+        <div className='flex flex-col'>
+          <label htmlFor="fname">Address</label>
+          <input
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            type="text"
+            id="fname"
+            required
+          />
+        </div>
+        </div>
+        </div>
+      
+      
     </div>
   )
 }
