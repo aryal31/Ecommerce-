b10b424cc4b65d15ea8f4a75704a2ec2dd5e996d
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-10-03 23:55:16 +0545

    simple design changes

--- src/Pages/Myaccount.jsx
+++ src/Pages/Myaccount.jsx
@@ -1,57 +1,95 @@
-import React from 'react'
+import React from "react";
+import Orangebutton from "../components/Orangebutton";
 
 const Myaccount = () => {
   return (
-    <div>
-      <div className='text-[20px] text-[#DB4444] font-semibold'>Edit your profile</div>
-      <div className='flex gap-[40px]'>
-        <div className='flex flex-col'>
+      <div className="flex items-center justify-center shadow-sm">
+    <div className="items-center">
+      <div className="text-[20px] text-[#DB4444] font-semibold">
+        Edit your profile
+      </div>
+      <div className="flex gap-[40px]">
+        <div className="flex flex-col">
           <label htmlFor="fname">First Name</label>
           <input
-            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none pl-4"
             type="text"
             id="fname"
+            placeholder="john"
             required
           />
         </div>
         <div>
-        <div className='flex flex-col'>
-          <label htmlFor="fname">First Name</label>
-          <input
-            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
-            type="text"
-            id="fname"
-            required
-          />
+          <div className="flex flex-col">
+            <label htmlFor="fname">Last name</label>
+            <input
+              className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] pl-4"
+              type="text"
+              id="fname"
+              placeholder="doe"
+              required
+            />
+          </div>
         </div>
-        </div>
-        </div>
-        <div className='flex gap-[40px]'>
-        <div className='flex flex-col'>
+      </div>
+      <div className="flex gap-[40px]">
+        <div className="flex flex-col">
           <label htmlFor="fname">Email</label>
           <input
-            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] pl-4"
             type="text"
+            placeholder="example@gmail.com"
             id="fname"
             required
           />
         </div>
         <div>
-        <div className='flex flex-col'>
-          <label htmlFor="fname">Address</label>
+          <div className="flex flex-col">
+            <label htmlFor="address">Address</label>
+            <input
+              className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] pl-3"
+              type="text"
+              id="address"
+              placeholder="123 street, 123"
+              required
+            />
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="flex flex-col">
+          <label htmlFor="password">Password</label>
           <input
-            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] foucs:none"
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] pl-4"
             type="text"
-            id="fname"
+            id="password"
+            placeholder="current password"
+            required
+          />
+          <input
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] pl-4"
+            type="text"
+            placeholder="New Password"
+            required
+
+          />
+
+<input
+            className="h-[50px] w-[330px] bg-[#F5F5F5] mt-[8px] mb-[32px] pl-4"
+            type="text"
+            id="password"
+            placeholder="Confirm new password"
             required
           />
         </div>
-        </div>
-        </div>
-      
-      
+      </div>
+      <div className="flex gap-3 items-center ">
+        <p>cancel</p>
+      <Orangebutton text='Save Changes'></Orangebutton>
+      </div>
     </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Myaccount
+export default Myaccount;
