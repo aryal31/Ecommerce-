23d8f7dd92563c8d1b40e3d8ed53864a6759d685
aryal31 <bipinaryal11@gmail.com>
2024-10-04 17:04:40 +0545

    about page design 50% done

--- src/Pages/Aboutpage.jsx
+++ src/Pages/Aboutpage.jsx
@@ -1,9 +1,82 @@
-import React from 'react'
+import React from "react";
+import Servicefeatures from "../components/Servicefeatures";
+import controller from "../assets/images/controller.png";
+import Salesdisplay from "../components/Salesdisplay";
+import person1 from "../assets/images/person1.png";
+import person2 from "../assets/images/person2.png";
+import person3 from "../assets/images/person3.png";
+import { CiInstagram, CiLinkedin, CiTwitter } from "react-icons/ci";
 
 const Aboutpage = () => {
   return (
-    <div>Aboutpage</div>
-  )
-}
+    <div>
+      <div className="flex">
+        <div>
+          <p className="text-[54px] font-semibold">Our story</p>
+          <p className="text-[16px]">
+            Launced in 2015, Exclusive is South Asia’s premier online shopping
+            makterplace with an active presense in Bangladesh. Supported by wide
+            range of tailored marketing, data and service solutions, Exclusive
+            has 10,500 sallers and 300 brands and serves 3 millioons customers
+            across the region.
+          </p>
+          <p className="text-[16px]">
+            Exclusive has more than 1 Million products to offer, growing at a
+            very fast. Exclusive offers a diverse assotment in categories
+            ranging from consumer.
+          </p>
+        </div>
+        <div className="h-[609px] w-[837]">
+          <img src={controller} alt="" />
+        </div>
+      </div>
+      <Salesdisplay></Salesdisplay>
+      <div className="flex justify-center gap-2">
+        <div className="h-[564px] w-[370px]"> 
+        <div className="h-[430px] w-[370px] bg-[#f5f5f5] flex justify-end items-center flex-col">
+          <img
+            src={person1}
+            alt=""
+            className=" h-[391px] w-[236px] "
+          />
+        </div>
+        <div>
+          <p className="text-[32px]">Tom cruise</p>
+          <p className="text-[16px]">founder and chairman</p>
+          <div className="flex gap-[10px] text-[20px]">
+          <CiTwitter />
+          <CiInstagram />
+          <CiLinkedin />
+          </div>
+        </div>
+        </div>
+        <div className="h-[430px] w-[370px] bg-[#f5f5f5] flex justify-end items-center flex-col">
+          <img
+            src={person2}
+            alt=""
+            className="w-[294px] h-[397px] "
+          />
+        </div>
+                <div>
+          <p className="text-[32px]">Tom cruise</p>
+          <p className="text-[16px]">founder and chairman</p>
+          <div className="flex gap-[10px] text-[20px]">
+          <CiTwitter />
+          <CiInstagram />
+          <CiLinkedin />
+          </div>
+        </div>
+        <div className="h-[430px] w-[370px] bg-[#f5f5f5] flex justify-end items-center flex-col">
+          <img
+            src={person3}
+            alt=""
+            className="w-[326px] h-[392] "
+          />
+        </div>
+      </div>
+      <Servicefeatures></Servicefeatures>
+    </div>
+  );
+};
 
-export default Aboutpage
+export default Aboutpage;
---
+++ src/components/Salesdisplay.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+import { PiMoneyWavy } from "react-icons/pi";
+import { RiCustomerService2Line } from "react-icons/ri";
+import { TbTruckDelivery } from "react-icons/tb";
+import { CiShop, CiDollar } from "react-icons/ci";
+import { FiShoppingBag } from "react-icons/fi";
+import { FaSackDollar } from "react-icons/fa6";
+
+const icondetails = [
+  {
+    name: "shop",
+    iname: CiShop,
+    quantity: "10.5k",
+    message: "Sellers active",
+  },
+  {
+    name: "money",
+    iname: CiDollar,
+    quantity: "33k",
+    message: "Monthly Product Sale",
+  },
+  {
+    name: "bag",
+    iname: FiShoppingBag,
+    quantity: "45.5k",
+    message: "Customers active in our site",
+  },
+  {
+    name: "sack",
+    iname: FaSackDollar,
+    quantity: "25k",
+    message: "Actual gross sale in our site",
+  },
+];
+
+const Salesdisplay = () => {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <div className="flex items-center justify-center gap-[30px]">
+        {icondetails.map((item, index) => {
+          const Icon = item.iname;
+          return (
+            <div
+              className={`flex items-center justify-center align-center flex-col w-[270px] h-[230px] border-[#d9d9d9] rounded-md border-[1.5px] ${
+                item.name === name
+                  ? "border-none bg-[#db4444] text-[white]"
+                  : ""
+              }`}
+              key={index}
+              onMouseEnter={() => {
+                setName(item.name);
+              }}
+              onMouseLeave={() => {
+                setName("");
+              }}
+            >
+              <div className="flex justify-center items-center">
+                <div
+                  className={`h-[58px] w-[58px] bg-black flex items-center justify-center rounded-full box-content border-[13px] border-opacity-30 ${
+                    item.name === name && "bg-black"
+                  }`}
+                >
+                  <Icon
+                    className={` text-[35px] ${
+                      item.name === name ? "text-black" : "text-white"
+                    }`}
+                  />
+                </div>
+              </div>
+              <p className="text-[32px] font-bold">{item.quantity}</p>
+              <p className="text-[16px]">{item.message}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Salesdisplay;
