012b40a73502195cbb22ef12dcc8cb1214d51b6d
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-10-04 08:48:07 +0545

    cart design updated

--- src/Pages/CartPage.jsx
+++ src/Pages/CartPage.jsx
@@ -5,7 +5,7 @@ import CartTotalbox from "../components/CartTotalbox";
 import Orangebutton from "../components/Orangebutton";
 const CartPage = () => {
   return (
-    <div>
+    <div className="container mx-auto">
       <Breadcrumb
         first="Account"
         second="My Account"
@@ -13,43 +13,40 @@ const CartPage = () => {
         fourth="View Cart"
         fifth="CheckOut"
       />
-      <div className="flex items-center justify-center flex-col">
-        <div className=""></div>
-        <div>
-          <div className="mb-3">
-            <div className="shadow flex justify-between w-[1170px] h-[72px]  text-[16px] mt-[10px] items-center ">
-              <p>Product</p>
-              <p>Price</p>
-              <p>Quantity</p>
-              <p>Subtotal</p>
-            </div>
+      <div className="mt-8">
+        <div className="">
+          <div className="grid grid-cols-4 h-[72px] text-16px items-center shadow">
+            <p>Product</p>
+            <p>Price</p>
+            <p>Quantity</p>
+            <p>Subtotal</p>
           </div>
           <Cartitemcard />
           <Cartitemcard />
+        </div>
 
-          <div className="flex justify-between mt-3">
-            <Buttonwithoutline text="Return to shop"></Buttonwithoutline>
-            <Buttonwithoutline text="Update Cart"></Buttonwithoutline>
+        <div className="flex justify-between mt-6">
+          <Buttonwithoutline text="Return to shop"></Buttonwithoutline>
+          <Buttonwithoutline text="Update Cart"></Buttonwithoutline>
+        </div>
+
+        <div className="flex justify-between mt-8">
+          <div className="flex gap-x-3">
+            <input
+              type="text"
+              placeholder="Coupon Code"
+              className="border-black border-[1px] rounded-sm h-[56px] pl-3"
+            />
+            <div className="mt-4">
+              <Orangebutton text="Apply Coupon" />
+            </div>
           </div>
-
-          <div className="flex justify-between mt-5">
-            <div className="flex gap-x-3">
-              <input
-                type="text"
-                placeholder="Coupon Code"
-                className="border-black border-[1px] rounded-sm h-[56px] pl-3"
-              />
-              <div className="mt-4">
-                <Orangebutton text="Apply Coupon" />
-              </div>
-            </div>
-            <div>
-              <CartTotalbox />
-            </div>
+          <div>
+            <CartTotalbox />
           </div>
         </div>
       </div>
-      <div />
+      <div/>
     </div>
   );
 };
--- src/components/Cartitemcard.jsx
+++ src/components/Cartitemcard.jsx
@@ -1,17 +1,22 @@
-import React from 'react'
-import controller from '../assets/images/controller.png'
+import React from "react";
+import controller from "../assets/images/controller.png";
 
 const Cartitemcard = () => {
   return (
     <>
-      <div className='flex justify-between items-center shadow  w-[1170px] h-[102px]'>
-      <div className='flex items-center'> <img src={controller} className='h-[50px]' alt="" /> <p>Dual Sense</p></div>
-      <p>$120</p>
-      <div className='border-[#d9d9d9] border-[1.5px] rounded-sm bg-none'><input type="number" className='w-[72px] h-[44px] ' /></div>
-      <p>$650</p>
+      <div className="grid grid-cols-4 items-center h-[72px] text-[16px] shadow">
+        <div className="flex items-center gap-2 ">
+          {" "}
+          <img src={controller} className="h-[50px]" alt="" /> <p>Dual Sense</p>
+        </div>
+        <p className="">$120</p>
+        <div className="flex">
+          <input type="number" className="border-[#d9d9d9] border-[1.5px] rounded-sm bg-none w-[72px] h-[44px] text-center" />
+        </div>
+        <p>$650</p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Cartitemcard
+export default Cartitemcard;
