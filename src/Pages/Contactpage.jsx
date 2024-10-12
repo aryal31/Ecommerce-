ff382e2e822c2f62044913516f830d7024979cb4
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-10-12 07:04:02 +0545

    contact page made responsive

--- src/Pages/Contactpage.jsx
+++ src/Pages/Contactpage.jsx
@@ -5,8 +5,8 @@ import Orangebutton from "../components/Orangebutton";
 
 const Contactpage = () => {
   return (
-    <div className="flex items-center justify-center gap-[30px] mt-5">
-      <div className="flex flex-col shadow w-[340px] h-[457px] px-[20px] py-[40px]">
+    <div className="flex flex-wrap items-center justify-center gap-[30px] mt-5">
+      <div className="flex flex-col shadow w-full max-w-[340px] h-[457px] px-[20px] py-[40px]">
         <div className="flex items-center gap-4">
           <div>
             <div className="flex items-center gap-4 mb-[24px]">
@@ -41,33 +41,33 @@ const Contactpage = () => {
         </div>
       </div>
 
-      <div className="flex flex-col shadow w-[800px] h-[457px] px-[32px] py-[40px]">
-        <div className="flex gap-2 mb-[32px]">
-          <input
+      <div className="flex flex-col shadow w-auto max-w-[800px] h-auto px-[32px] py-[40px] flex-wrap">
+        <div className="flex flex-row gap-2 mb-[32px] flex-wrap">
+          <div><input
             type="text"
             className="w-[235px] h-[50px] bg-[#F5F5F5] pl-5 focus:outline-none"
             placeholder="Your Name"
             required
-          />
-          <input
+          /></div>
+          <div><input
             type="text"
             className="w-[235px] h-[50px] bg-[#F5F5F5] pl-5 focus:outline-none"
             placeholder="Your Email"
             required
-          />
-          <input
+          /></div>
+          <div><input
             type="text"
             className="w-[235px] h-[50px] bg-[#F5F5F5] pl-5 focus:outline-none"
             placeholder="Your Phone"
             required
-          />
+          /></div>
         </div>
-        <div>
+        <div className="w-full">
           <textarea
             name=""
             id=""
             placeholder="Your Massage"
-            className="pl-5 pt-4 bg-[#F5F5F5] focus:outline-none w-[737px] h-[207px]"
+            className="pl-5 pt-4 w-full bg-[#F5F5F5] focus:outline-none h-[207px] resize-none"
           ></textarea>
         </div>
         <div className="self-end mt-7">
