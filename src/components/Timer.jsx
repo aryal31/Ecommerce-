db37bb4e250f66a3e15300085788d435e64ea29a
aryal31 <113261195+aryal31@users.noreply.github.com>
2024-11-25 21:24:41 +0545

    updated timer component

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
-const Timer = () => {
-  return <div></div>;
+const CountdownTimer = ({ days, hours, minutes, seconds, className }) => {
+  const [timeLeft, setTimeLeft] = useState({
+    days,
+    hours,
+    minutes,
+    seconds
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeLeft((prevState) => {
+        let newSeconds = prevState.seconds - 1;
+        let newMinutes = prevState.minutes;
+        let newHours = prevState.hours;
+        let newDays = prevState.days;
+
+        if (newSeconds === -1) {
+          newSeconds = 59;
+          newMinutes--;
+        }
+
+        if (newMinutes === -1) {
+          newMinutes = 59;
+          newHours--;
+        }
+
+        if (newHours === -1) {
+          newHours = 23;
+          newDays--;
+        }
+
+        return {
+          days: newDays,
+          hours: newHours,
+          minutes: newMinutes,
+          seconds: newSeconds
+        };
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);  
+  }, []);
+
+  return (
+<div className={`flex justify-center space-x-4 ${className}`}>
+   <div>
+   <div className="text-sm">Days</div>
+   <div className="text-4xl font-bold">{timeLeft.days}<span className='text-[#db4444]'> : </span></div>
+   </div>
+   <div>
+   <div className="text-sm">Hours</div>
+   <div className="text-4xl font-bold">{timeLeft.hours}<span className='text-[#db4444]'> : </span></div>
+   </div>
+   <div>
+   <div className="text-sm">Minutes</div>
+   <div className="text-4xl font-bold">{timeLeft.minutes}<span className='text-[#db4444]'> : </span></div>
+   </div>
+   <div>
+   <div className="text-sm">Seconds</div>
+   <div className="text-4xl font-bold">{timeLeft.seconds}</div>
+   </div>
+    </div>
+  );
 };
 
-export default Timer;
+export default CountdownTimer;
--- src/components/features/home/Flashsale.jsx
+++ src/components/features/home/Flashsale.jsx
@@ -3,6 +3,7 @@ import Arrow from "../../common/Arrow";
 import Wrapper from "../../common/Wrapper";
 import Itemcard from "../product/Itemcard";
 import Orangebutton from "../../common/Orangebutton";
+import CountdownTimer from "../../Timer";
 
 const Flashsale = ({ className }) => {
   return (
@@ -10,6 +11,7 @@ const Flashsale = ({ className }) => {
       <Wrapper>
         <div className={`flex justify-between ${className}`}>
           <Heading date="Today's" title="flash sales" />
+          <CountdownTimer days={3} hours={23} minutes={19} seconds={56} className={'mt-[46px] mr-[400px]'} />
           <Arrow />
         </div>
         <div className="flex flex-col justify-center items-center">
@@ -20,7 +22,10 @@ const Flashsale = ({ className }) => {
             <Itemcard />
           </div>
           <div>
-            <Orangebutton text="View all products" className='h-[50px] w-[200px]' />
+            <Orangebutton
+              text="View all products"
+              className="h-[50px] w-[200px]"
+            />
           </div>
         </div>
       </Wrapper>
