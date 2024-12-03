e495e4c3cdc679efa2307fa7639fd2ecc5524dcb
aryal31 <bipinaryal11@gmail.com>
2024-12-03 23:36:13 +0545

    minor changes

--- src/components/features/about/Salesdisplay.jsx
+++ src/components/features/about/Salesdisplay.jsx
@@ -1,4 +1,4 @@
-import  { useState } from "react";
+import { useState } from "react";
 import { CiShop, CiDollar } from "react-icons/ci";
 import { FiShoppingBag } from "react-icons/fi";
 import { FaSackDollar } from "react-icons/fa6";
@@ -34,40 +34,36 @@ const Salesdisplay = () => {
   const [name, setName] = useState("");
 
   return (
-    <div>
-      <div className="flex items-center justify-center gap-[30px] flex-wrap">
+    <div className="p-4">
+      <div className="flex items-center justify-center gap-6 flex-wrap">
         {icondetails.map((item, index) => {
           const Icon = item.iname;
           return (
             <div
-              className={`flex items-center justify-center align-center flex-col w-[270px] h-[230px] border-[#d9d9d9] rounded-md border-[1.5px] ${
+              className={`flex items-center justify-center align-center flex-col md:w-[270px] md:h-[230px] w-[150px] h-[180px] border-[#d9d9d9] rounded-md border-[1.5px] ${
                 item.name === name
-                  ? "border-none bg-[#db4444] text-[white]"
+                  ? "border-none bg-[#db4444] text-white"
                   : ""
-              }`}
+              } transition-all duration-300`}
               key={index}
-              onMouseEnter={() => {
-                setName(item.name);
-              }}
-              onMouseLeave={() => {
-                setName("");
-              }}
+              onMouseEnter={() => setName(item.name)}
+              onMouseLeave={() => setName("")}
             >
-              <div className="flex justify-center items-center">
+              <div className="flex justify-center items-center mb-2">
                 <div
-                  className={`h-[58px] w-[58px] bg-black flex items-center justify-center rounded-full box-content border-[13px] border-opacity-30 ${
-                    item.name === name && "bg-white"
+                  className={`md:h-[58px] md:w-[58px] h-[40px] w-[40px] bg-black flex items-center justify-center rounded-full box-content border-[8px] border-opacity-30 ${
+                    item.name === name ? "bg-white" : ""
                   }`}
                 >
                   <Icon
-                    className={` text-[35px] ${
+                    className={`md:text-[35px] text-[24px] ${
                       item.name === name ? "text-black" : "text-white"
                     }`}
                   />
                 </div>
               </div>
               <p className="text-[32px] font-bold">{item.quantity}</p>
-              <p className="text-[16px]">{item.message}</p>
+              <p className="text-[16px] text-center">{item.message}</p>
             </div>
           );
         })}
